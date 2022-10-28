74fb56853c2d073262b3534aaf6cf77ece8b34ad
ChoLaamY <cy2617@columbia.edu>
2022-10-28 13:56:51 -0400

    initial commit

---
+++ cipher_cy2617/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use cipher_cy2617 in a project::
+
+    import cipher_cy2617
---
+++ cipher_cy2617/tests/test_cipher_cy2617.py
@@ -0,0 +1,5 @@
+from cipher_cy2617 import __version__
+from cipher_cy2617 import cipher_cy2617
+
+def test_version():
+    assert __version__ == 0.1.0
